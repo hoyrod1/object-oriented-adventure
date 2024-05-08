77fb44369ac5ce1128a24a8eb84faaf2bd7dcd7e
Rodney St Cloud <hoyrod1@aol.com>
2024-05-07 20:25:54 -0400

    Completed part 3 added a energyDrink property to the Adventurer Class and created a Companion Class with a constructor method with three properties food, water and watch and created a foodSchedule method. Completed Part 4 adding a static MAX_HEALTH property, a static ROLES array to the Adventure class and a if statement to check if the role entered is included in the static ROLES array

--- object-oriented-adventure.js
+++ object-oriented-adventure.js
@@ -35,6 +35,7 @@ adventurer.roll();
 console.log("==================== Part 2: Class Fantasy ====================");
 //===========================================================================//
 class Character {
+  static MAX_HEALTH = 100;
   constructor(name) {
     this.name = name;
     this.health = 100;
@@ -45,40 +46,68 @@ class Character {
     console.log(`${this.name} rolled a ${result}.`);
   }
 }
-const robin = new Character("Robin");
-robin.inventory = ["sword", "potion", "artifact"];
-robin.companion = new Character("Leo");
-robin.companion.type = "Cat";
-robin.companion.inventory = ["fury hat", "nail clippers"];
-robin.companion.companion = new Character("Frank");
-robin.companion.companion.type = "Flea";
-robin.companion.companion.inventory = ["small hat", "sunglasses"];
-// console.log(robin);
-robin.inventory.forEach((item) => {
-  console.log(item);
-});
-robin.roll();
-robin.companion.inventory.forEach((item) => {
-  console.log(item);
-});
-robin.roll();
-robin.companion.companion.inventory.forEach((item) => {
-  console.log(item);
-});
+// const robin = new Character("Robin");
+// robin.inventory = ["sword", "potion", "artifact"];
+// robin.companion = new Character("Leo");
+// robin.companion.type = "Cat";
+// robin.companion.inventory = ["fury hat", "nail clippers"];
+// robin.companion.companion = new Character("Frank");
+// robin.companion.companion.type = "Flea";
+// robin.companion.companion.inventory = ["small hat", "sunglasses"];
+// // console.log(robin);
+// robin.inventory.forEach((item) => {
+//   console.log(item);
+// });
+// robin.roll();
+// robin.companion.inventory.forEach((item) => {
+//   console.log(item);
+// });
+// robin.roll();
+// robin.companion.companion.inventory.forEach((item) => {
+//   console.log(item);
+// });
 //---------------------------------------------------------------------------//
 console.log("==================== Part 3: Class Features ====================");
 //===========================================================================//
 class Adventurer extends Character {
+  static ROLES = ["Fighter", "Healer", "Wizard", "Mind Reader"];
   constructor(name, role) {
     super(name);
     // Adventurers have specialized roles.
+    this.energyDrink = 100;
+    if (!Adventurer.ROLES.includes(role)) {
+      throw new Error("This is not a valid role");
+    }
     this.role = role;
     // Every adventurer starts with a bed and 50 gold coins.
     this.inventory.push("bedroll", "50 gold coins");
   }
   // Adventurers have the ability to scout ahead of them.
   scout() {
-    console.log(`${this.name} is scouting ahead...`);
+    console.log(`${this.name} the ${this.role} is scouting ahead...`);
     super.roll();
   }
 }
+const robin = new Adventurer("Robin", "Fighter");
+robin.scout();
+// console.log(robin);
+// console.log(adventurer2);
+//---------------------------------------------------------------------------//
+console.log("==================== Part 4: Class Uniforms ====================");
+//===========================================================================//
+class Companion {
+  constructor(food, water, watch) {
+    this.food = food;
+    this.water = water;
+    this.watch = watch;
+  }
+  // Method determines when it is time to eat
+  foodSchedule(timeToEat) {
+    if (timeToEat === this.watch) {
+      console.log(`It is time to eat ${this.food} and drink ${this.water}`);
+    }
+  }
+}
+//---------------------------------------------------------------------------//
+console.log("================== Part 5: Gather your Party ==================");
+//===========================================================================//
